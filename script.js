0ab4467b0ad0e593bd600e30d31ad3730ac2a19c
ruta-tamosiunaite <94722452+ruta-tamosiunaite@users.noreply.github.com>
2023-11-17 14:34:29 +0100

    nav, photo css, js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function showMessage() {
     alert("+33 6 16 31 99 96");
 }
+
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 100) { // Adjust the 100 value as needed
+        navbar.style.backgroundColor = '#555'; // New background color on scroll
+    } else {
+        navbar.style.backgroundColor = 'transparent'; // Back to transparent
+    }
+});
