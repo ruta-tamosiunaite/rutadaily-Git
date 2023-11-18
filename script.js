b75a907f72f17f01fdf700b818227a8e50838f8f
ruta-tamosiunaite <94722452+ruta-tamosiunaite@users.noreply.github.com>
2023-11-18 15:51:34 +0100

    some updates

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-function showMessage() {
-    alert("+33 6 16 31 99 96");
-}
-
 window.addEventListener('scroll', function() {
-    const navbar = document.getElementById('navbar');
-    if (window.scrollY > 100) { // Adjust the 100 value as needed
-        navbar.style.backgroundColor = '#555'; // New background color on scroll
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY > 0) {
+        navbar.classList.add('visible');
     } else {
-        navbar.style.backgroundColor = 'transparent'; // Back to transparent
+        navbar.classList.remove('visible');
     }
 });
